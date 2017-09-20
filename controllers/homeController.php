186afb3108284cf93b9d73cda949702b3256a4bc
Gerson Arbigaus <gerson87@gmail.com>
2017-09-19 22:37:24 -0300

    Add README file

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -8,29 +8,6 @@ class homeController extends Controller {
 	public function index(){
 		$data = array();
 
-		// $dados_clientes = array(
-		// 	"name" => 'Fernando Arbigaus',
-		// 	"sexo" => 'masculino'
-		// 	);
-
-		// Clientes::Create($dados_clientes);
-
-		// if(Clientes::getResult()):
-		// 	echo "O último cliente foi ".Clientes::getResult();
-		// endif;
-
-		// echo "<pre>";
-		// print_r(Clientes::ReadByField('id_cliente', 1));
-		// print_r(Clientes::ReadAll());
-
-		// Clientes::Update($dados_clientes,'id_cliente',2);
-
-
-		// Clientes::Delete("id_cliente",2);
-		
-		// $teste = array("id_cliente" => 3);
-
-		// print_r(Clientes::FullRead("SELECT * FROM tab_clientes WHERE id_cliente = :id_cliente", $teste));
 
 		$this->loadTemplate('home', $data);
 
