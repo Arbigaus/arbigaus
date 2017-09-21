bdf0dc201d44b190b1ca8cf1fc6a229cbde9314b
Gerson Arbigaus <gerson87@gmail.com>
2017-09-20 22:13:45 -0300

    Pagination Class finished

--- models/Pagination.php
+++ models/Pagination.php
@@ -5,7 +5,7 @@ class Pagination extends Model {
   private static $maxPerPage = 2;
   private static $page = 'page';
   private static $startPage;
-  private static $maxLinks;
+  private static $maxLinks = 2;
   private static $query = "SELECT * FROM ";
   private static $queryCount = null;
   private static $places = array();
@@ -77,7 +77,7 @@ class Pagination extends Model {
   public static function createPagination($where = null, $orderBy = null){
     // $inicio = (($max_pag * $pagina) - $max_pag);
     self::$startPage = ((self::getMaxPerPage() * self::getPager()) - self::getMaxPerPage());
-    self::$startPage = (self::$startPage >= 1 ? self::$startPage : 1);
+    self::$startPage = (self::$startPage >= 1 ? self::$startPage : 0);
 
     if($where == null && $orderBy == null){
       //  "SELECT * FROM tab_clients ORDER BY id_client ASC LIMIT $inicio,$max_pag";
@@ -95,7 +95,71 @@ class Pagination extends Model {
     }
     return Pagination::getResult();
 
-  }
+  }  // Fim do método Create Pagination
+
+  public static function createLinks(){
+    $links = array();
+    self::$queryCount = str_replace("*","COUNT(*) AS Total",self::$query);
+    // if(strrpos(self::$queryCount,"ORDER")){
+    //   self::$queryCount = substr(self::$queryCount, 0,strpos(self::$queryCount,"ORDER"));
+    // }else{
+      self::$queryCount = substr(self::$queryCount, 0,strpos(self::$queryCount,"LIMIT"));
+    // }
+
+    if(strrpos(self::$queryCount, "WHERE")){
+      Pagination::FullRead(self::$queryCount, self::getPlaces());
+    }else{
+      Pagination::FullRead(self::$queryCount);
+    }
+
+  		$total_registro = Pagination::getResult()[0]['Total'];
+  		$total_paginas = ceil($total_registro / self::getMaxPerPage());
+
+      if(self::getPager() > $total_paginas || self::getPager() < 1){
+        header("Location: ".self::returnPageValid(self::getPage())."?".self::getPage()."=".$total_paginas);
+      }else{
+
+      	if($total_registro > self::getMaxPerPage()){
+  			if(self::getPager() > 1){
+  				$links[] = '<ul class="pagination"><li><a href="?'.self::getPage().'=1">Primeira Página</a></li>';
+  			}else{
+          $links[] = '<ul class="pagination">';
+        }
+  			// TODO: Imprimindo paginação, duas antes da página atual.
+  			for($i = self::getPager() - self::getMaxLinks(); $i <= self::getPager() - 1; $i++){
+  				if($i >= 1){
+  					$links[] = '<li><a href="?'.self::getPage().'='.$i.'">'.$i.'</a></li>';
+  				}
+  			}
+  			// Imprimir página atual
+  			$links[] = '<li class="active"><span>'.self::getPager().'</span>';
+
+  			for ($i = self::getPager() + 1; $i <= self::getPager() + self::getMaxLinks() ; $i++) {
+  				if($i <= $total_paginas){
+  					$links[] = '<li><a href="?'.self::getPage().'='.$i.'">'.$i.'</a></li>';
+  				}
+  			}
+  			// Imprimir última página
+  			if(self::getPager() != $total_paginas){
+  				$links[] = '<li><a href="?'.self::getPage().'='.$total_paginas.'">Última Página</a></li><ul>';
+        }else{
+          $links[] = '<ul>';
+        }
+      }
+
+    }
+    return $links;
+  } // Fim do método createLinks
+
+  private static function returnPageValid($name_pager){
+    $URL = $_SERVER['HTTP_HOST'];
+    $url = "http://".$URL.$_SERVER['REQUEST_URI'];
+
+    $clearpage = $url;
+    $clearpage = substr($clearpage, 0, strpos($clearpage,"?".$name_pager));
+    return $clearpage;
+
+  }// Fim do método Return Page Valid
 
 
-}
+} // Fim da Classe Pagination
